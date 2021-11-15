1d78f1c14f134b770ed17777991101bf297b01f4
Ed Oakes <ed.nmi.oakes@gmail.com>
2021-11-15 14:02:43 -0800

    fix

--- preprocess.py
+++ preprocess.py
@@ -8,11 +8,17 @@ if len(sys.argv) > 1:
 
 import ray
 
+start = time.time()
+
 print("Initializing Ray...")
 ray.init()
 
 print("Doing feature preprocessing.")
 ds = ray.data.range(10 if SMOKE_TEST else 100000000)
 ds = ds.map(lambda x: x * 2)
+
+extra_time = 30 - (time.time() - start)
+if not SMOKE_TEST:
+    time.sleep(max(0, extra_time))
 
 print("Succeeded.")
