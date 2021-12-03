f22065de71cef6744f5cea7d817473837cda4565
Ed Oakes <ed.nmi.oakes@gmail.com>
2021-12-03 10:02:49 -0600

    fix

--- deploy_counter.py
+++ deploy_counter.py
@@ -1,9 +1,17 @@
+import argparse
+import logging
+
 from fastapi import FastAPI
 
 import ray
 from ray import serve
 
 serve.start(detached=True)
+
+logger = logging.getLogger(__name__)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--max-count", type=int, required=False, default=10)
 
 app = FastAPI()
 
@@ -12,7 +20,8 @@ app = FastAPI()
 class Counter:
     def __init__(self, max_count: int = 10):
         self._count = 0
-        self._max_count = 10
+        self._max_count = max_count
+        logger.info(f"Starting with max_count: {max_count}")
 
     @app.get("/")
     def get(self):
@@ -33,4 +42,6 @@ class Counter:
         if self._count >= self._max_count:
             raise Exception("Count exceeded maximum.")
 
-Counter.deploy()
+if __name__ == "__main__":
+    args = parser.parse_args()
+    Counter.deploy(max_count=args.max_count)
